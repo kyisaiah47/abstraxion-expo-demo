274cceaaa48169d16fd8f06e90ea81e05c6a43e4
Ash <ash@burnt.com>
2025-05-01 14:26:17 -0700

    pull initialization out of component loop

--- components/SpotifyProfile.tsx
+++ components/SpotifyProfile.tsx
@@ -7,14 +7,12 @@ import { useEffect, useState } from 'react'
 import { Button } from 'react-native'
 import {ThemedText} from "@/components/ThemedText";
 
+init("example-app-e0604831-e8a1-4674-a098-6e85315984a2", false, OpacityEnvironment.Production, true)
+
 
 export function SpotifyProfile(): React.JSX.Element {
 
     const [result, setResult] = useState("")
-
-    useEffect(() => {
-        init("example-app-e0604831-e8a1-4674-a098-6e85315984a2", false, OpacityEnvironment.Production, true)
-    }, [])
 
 
     const getSpotifyProfile = async evt => {
